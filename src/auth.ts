5edaff2f063779f3a504246e40f425a9ab0b9f6e
Nikhil Gupta <nikhil.gupta5667@gmail.com>
2024-11-20 10:59:44 +0000

    Initial commit from Create Next App

---
+++ src/auth.ts
@@ -0,0 +1,8 @@
+import { prisma } from '@/utils/db'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import NextAuth from 'next-auth'
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+	adapter: PrismaAdapter(prisma),
+	providers: [],
+})
